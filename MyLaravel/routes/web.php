a21763789a77dcb940d55f8ddd96c13e9aa10a96
Kaowweng <nnvvvton@gmail.com>
2025-01-14 13:39:36 +0700

    new

--- MyLaravel/routes/web.php
+++ MyLaravel/routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\MyController;
 Route::get('/', function () {
     return view('welcome');
 });
 Route ::get('/hello', function () {
-    return "<h1>Hello World<h1>";
+    return "<h1>hi<h1>";;
 });
+Route::get('/mylaravel/{id?}',[Mycontroller::class,'myfunction']);
+
+Route::post('/mylaravel/{id?}',[Mycontroller::class,'myfunction']);
